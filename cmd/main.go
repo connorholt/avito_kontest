f0d71c8ab46f39d8ff270ec90ffd197ffe2677f4
Kamil <kamilggss@gmail.com>
2024-04-08 18:02:31 +0300

    Init

---
+++ cmd/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"log"
+	"net/http"
+	"os"
+
+
+	_ "github.com/lib/pq"
+)
+
+//	@title			Vk_app
+//	@version		1.0
+//	@description	My Application
+//  @schemes 		http
+//	@host			localhost:5000
+//	@BasePath		/
+//  @securityDefinitions.apikey ApiKeyAuth
+//  @in header
+//  @name Authorization
+
+type application struct {
+	infoLog     *log.Logger
+	errorLog    *log.Logger
+	people      storage.ActorStorageInterface
+	films       storage.FilmStorageInterface
+	users       storage.UserStorageInterface
+	filmsActors storage.FilmActorStorageInterface
+}
+
+func main() {
+	db_c := config.GetConfig()
+
+	addr := flag.String("addr", ":5000", "HTTP network address")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable", db_c.Host, db_c.Port, db_c.User, db_c.Password, db_c.DB_name)
+
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime)
+
+	db, err := storage.OpenDB(psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	app := application{
+		infoLog:     infoLog,
+		errorLog:    errorLog,
+		people:      &storage.ActorStorage{DB: db},
+		films:       &storage.FilmStorage{DB: db},
+		users:       &storage.UserStorage{DB: db},
+		filmsActors: &storage.FilmActorStorage{DB: db},
+	}
+
+	srv := &http.Server{
+		Addr:    *addr,
+		Handler: app.routes(),
+	}
+	infoLog.Println("Starting server on", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
